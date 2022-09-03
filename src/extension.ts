1579d72b28023f4357ad564b4d1c6bc7a040ed4f
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2022-09-02 21:47:27 -0400

    Implement selection revealing and add comments to the code

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-
   type Mode = 'big' | 'xl' | 'normal';
 
   let mode: Mode = 'normal';
@@ -25,6 +18,25 @@ export function activate(context: vscode.ExtensionContext) {
 
   const enlargeCallback = (mode_: Mode, magnitude: number): (() => void) => {
     return () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage('Could not detect an active editor.');
+        return;
+      }
+
+      // Dependeng on the current mode, set mode to the appropriate alternate
+      // mode.
+      //
+      // The graph below illustrates the mode transformations, where X
+      // represents the current mode and Y represents the target mode, the
+      // intersection represents the new mode.
+      //
+      // |        | Normal | Big    | XL     |
+      // |--------+--------+--------+--------|
+      // | Normal |   X    | Big    | XL     |
+      // | Big    |   X    | Normal | XL     |
+      // | XL     |   X    | Big    | Normal |
+
       if (mode === 'normal') {
         mode = mode_;
       } else if (mode === mode_) {
@@ -38,18 +50,30 @@ export function activate(context: vscode.ExtensionContext) {
       } else {
         vscode.commands.executeCommand('editor.action.fontZoomReset');
       }
+
+      // Ensure that the cursor is still on the page after a mode change.
+
+      const range = editor.selection;
+      if (range) {
+        editor.revealRange(range);
+      }
     };
   };
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
+  //~~~~~~~~~~~~~~~//
+  // BIG FONT MODE //
+  //~~~~~~~~~~~~~~~//
+
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'big-font-mode.bigFontMode',
       enlargeCallback('big', 4)
     )
   );
+
+  //~~~~~~~~~~~~~~//
+  // XL FONT MODE //
+  //~~~~~~~~~~~~~~//
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
@@ -59,5 +83,4 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
